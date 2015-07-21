fb2ac7b7b26de123b263a291391d278b2d50bad9
Gosuke Miyashita <gosukenator@gmail.com>
2015-07-21 16:55:12 +0900

    Display name

--- destroy_all_vm_on_digitalocean.rb
+++ destroy_all_vm_on_digitalocean.rb
@@ -6,5 +6,14 @@ Digitalocean.client_id = ENV['DIGITALOCEAN_CLIENT_ID']
 Digitalocean.api_key   = ENV['DIGITALOCEAN_API_KEY']
 
 Digitalocean::Droplet.all.droplets.each do |d|
+  puts d.name
   Digitalocean::Droplet.destroy(d.id)
 end
+
+
+
+
+
+
+
+
